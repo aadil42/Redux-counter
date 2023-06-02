705e02ebe427a40d49b7e50f14df61162331d732
aadil42 <adilkelawala@gmail.com>
2023-06-02 12:30:41 +0530

    dispatching actions and increamenting and decreamenting the counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,30 @@
 import classes from './Counter.module.css';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
   const counter = useSelector(state => state.counter);
+
+  const dispatch = useDispatch();
+
+  const incrementHandle = () => {
+    dispatch({type: 'INCREAMENT'});
+  }
+
+  const decreamentHandle = () => {
+    dispatch({type: 'DECREAMENT'});
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{ counter }</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div className={classes['button-wrapper']}>
+        <button onClick={incrementHandle}>+</button>
+        <button onClick={decreamentHandle}>-</button>
+      </div>
     </main>
   );
 };
