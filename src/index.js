ff9ac1c81e68b4edc82166a0b3bbe74caf55eb9a
aadil42 <adilkelawala@gmail.com>
2023-06-02 11:45:12 +0530

    connecting the store with react app in index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// get the provider from redux-react
+import { Provider } from 'react-redux';
+
+// get the store 
+import store from './store/index';
 
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
