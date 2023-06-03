5b6e9302dbea9aa38e8759b5338bb1ff50951e4b
aadil42 <adilkelawala@gmail.com>
2023-06-03 17:04:56 +0530

    added login logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,52 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+// importinat auth actions
+import { authActions } from '../store';
+
+
 import classes from './Header.module.css';
 
+
 const Header = () => {
+
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const loginHandler = () => {
+    dispatch(authActions.logIn());
+  }
+  const logoutHandler = () => {
+    dispatch(authActions.logOut());
+  }
+
+  let nav = <nav>
+              <ul>
+                <li>
+                <button onClick={loginHandler}>LogIn</button>
+                </li>
+              </ul>
+            </nav>;
+
+  if(isAuthenticated) {
+    nav =  <nav>
+                  <ul>
+                    <li>
+                      <a href='/'>My Products</a>
+                    </li>
+                    <li>
+                      <a href='/'>My Sales</a>
+                    </li>
+                    <li>
+                    <button onClick={logoutHandler}>Logout</button>
+                    </li>
+                  </ul>
+          </nav>;
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {nav}
     </header>
   );
 };
